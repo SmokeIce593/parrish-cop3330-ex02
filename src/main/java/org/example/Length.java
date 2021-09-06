f9287611b8207b97d984c93eef45d14af1f08b01
SmokeIce593 <37118809+SmokeIce593@users.noreply.github.com>
2021-09-06 16:41:20 -0400

    finished

--- src/main/java/org/example/Length.java
+++ src/main/java/org/example/Length.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
 package org.example;
 import java.util.Scanner;
 
@@ -7,7 +12,7 @@ class Length {
         Scanner input = new Scanner(System.in);
 
         System.out.print( "What is the input string?" );
-        String name = input.next();
+        String name = input.nextLine();
 
         System.out.println(name + " has " + name.length() + " characters");
 
